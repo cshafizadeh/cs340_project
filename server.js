fdf2183959676f6cd7da1719a712a9bfcd103a72
cshafizadeh <shafizac@oregonstate.edu>
2022-04-26 19:09:55 -0700

    updated files

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ var fs = require("fs");
 var http = require('http');
 var PORT = 4000;
 
+var hdrData
 var idxData
 var cssData
 var pplData
+
+var hdrData = fs.readFile('./header.html', 'utf8', function(err, data){
+	if(err){
+		throw err
+	}
+	hdrData = data
+})
 
 var indexData = fs.readFile('./index.html', 'utf8', function(err, data){
 	if(err){
@@ -33,6 +41,7 @@ var server = http.createServer(function(req, res) {
 	if(req.url === "/"){
 		res.statusCode = 200
 		res.setHeader('Content-Type', 'text/html')
+        res.write(hdrData)
 		res.write(idxData)
 		res.end()
 		console.log("main page loaded")
@@ -40,6 +49,7 @@ var server = http.createServer(function(req, res) {
     else if(req.url === "/people"){
         res.statusCode = 200
 		res.setHeader('Content-Type', 'text/html')
+        res.write(hdrData)
 		res.write(pplData)
 		res.end()
 		console.log("people page loaded")
