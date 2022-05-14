3ad99647c8dbe931144ff255c1888408436f11e2
laraje <laraje@oregonstate.edu>
2022-05-13 17:26:57 -0700

    Added description for each page, and updated placeholder text.

--- public/personTemplate.js
+++ public/personTemplate.js
@@ -8,14 +8,14 @@ templates['person'] = template({"compiler":[8,">= 4.3.0"],"main":function(contai
         return undefined
     };
 
-  return " <section class=\"person\">\n    <div class=\"first-name\">\n        "
+  return " <section class=\"person\">\r\n    <div class=\"first-name\">\r\n        "
     + alias4(((helper = (helper = lookupProperty(helpers,"firstname") || (depth0 != null ? lookupProperty(depth0,"firstname") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"firstname","hash":{},"data":data,"loc":{"start":{"line":3,"column":8},"end":{"line":3,"column":21}}}) : helper)))
-    + "\n    </div>\n    <div class=\"last-name\">\n        "
+    + "\r\n    </div>\r\n    <div class=\"last-name\">\r\n        "
     + alias4(((helper = (helper = lookupProperty(helpers,"lastname") || (depth0 != null ? lookupProperty(depth0,"lastname") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"lastname","hash":{},"data":data,"loc":{"start":{"line":6,"column":8},"end":{"line":6,"column":20}}}) : helper)))
-    + "\n    </div>\n    <div class=\"cust_id\">\n        "
+    + "\r\n    </div>\r\n    <div class=\"cust_id\">\r\n        "
     + alias4(((helper = (helper = lookupProperty(helpers,"cust_id") || (depth0 != null ? lookupProperty(depth0,"cust_id") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"cust_id","hash":{},"data":data,"loc":{"start":{"line":9,"column":8},"end":{"line":9,"column":19}}}) : helper)))
-    + "\n    </div>\n    <div class=\"order\">\n        <a href=\"orders/"
+    + "\r\n    </div>\r\n    <div class=\"order\">\r\n        <a href=\"orders/"
     + alias4(((helper = (helper = lookupProperty(helpers,"firstname") || (depth0 != null ? lookupProperty(depth0,"firstname") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"firstname","hash":{},"data":data,"loc":{"start":{"line":12,"column":24},"end":{"line":12,"column":37}}}) : helper)))
-    + "\">Orders</a>\n    </div>\n    <div class=\"delete\">\n        <button id=\"delete-person\">Delete</button>\n    </div>\n    \n</section>";
+    + "\">Orders</a>\r\n    </div>\r\n    <div class=\"delete\">\r\n        <button id=\"delete-person\">Delete</button>\r\n    </div>\r\n    \r\n</section>";
 },"useData":true});
 })();
