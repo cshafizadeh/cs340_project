1e803e4b35f2d86ccdf56890641cd1409f266056
cshafizadeh <shafizac@oregonstate.edu>
2022-05-24 14:16:04 -0700

    added insert functionality

--- order.js
+++ order.js
@@ -14,18 +14,63 @@ module.exports = function(){
         });
     }
 
-    router.get('/', function(req, res){
+    function getItem(res, mysql, context, complete){
+        mysql.pool.query("SELECT itemId, itemTitle, itemDesc FROM item", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.item = results;
+            console.log(results)
+            complete();
+        });
+    }
+
+    function getPeople(res, mysql, context, complete){
+        mysql.pool.query("SELECT customerId, customerFirstName, customerLastName FROM customer", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.people = results;
+            console.log(results)
+            complete();
+        });
+    }
+
+    router.get('/orders', function(req, res){
         var callbackCount = 0;
         var context = {};
         //context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
         getOrder(res, mysql, context, complete);
+        getItem(res, mysql, context, complete);
+        getPeople(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 1){
+            if(callbackCount >= 3){
                 res.render('orderPage', context);
             }
         }
     });
+
+    router.post('/orders', function(req, res){
+        console.log('OK', req.body.order)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "insert into `order`(orderDate, customerId) values(?,?);";
+        var inserts = [req.body.orderDate, req.body.customerId];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                console.log('why?')
+                res.redirect('/orders');
+            }
+        });
+    });
+
     return router;
 }();
--- people.js
+++ people.js
@@ -14,7 +14,7 @@ module.exports = function(){
         });
     }
 
-    router.get('/', function(req, res){
+    router.get('/people', function(req, res){
         var callbackCount = 0;
         var context = {};
         //context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
@@ -27,5 +27,24 @@ module.exports = function(){
             }
         }
     });
+
+    router.post('/people', function(req, res){
+        console.log('OK', req.body.person)
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO customer (customerFirstName, customerLastName) VALUES (?,?)";
+        var inserts = [req.body.customerFirstName, req.body.customerLastName];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/people');
+            }
+        });
+    });
+
+
     return router;
 }();
