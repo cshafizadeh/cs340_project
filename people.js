1a8ece9e3960831f782bf4736ce8280cad818c07
cshafizadeh <shafizac@oregonstate.edu>
2022-06-03 17:40:30 -0700

    added comments to people.js

--- people.js
+++ people.js
@@ -2,9 +2,9 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getAllPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT customerId, customerFirstName, customerLastName FROM customer", function(error, results, fields){
-            if(error){
+    function getAllPeople(res, mysql, context, complete){ //retrieves data for all customers
+        mysql.pool.query("SELECT customerId, customerFirstName, customerLastName FROM customer", function(error, results, fields){ //SQl query to select data for all customers
+            if(error){ //checks if there is an error in creating customer, if so error is logged
                 res.write(JSON.stringify(error));
                 res.end();
             }
@@ -14,11 +14,11 @@ module.exports = function(){
         });
     }
 
-    function searchPeople(res, mysql, context, complete, search) {
+    function searchPeople(res, mysql, context, complete, search) { //retreives data on specific customer selected by user search
         mysql.pool.query("SELECT customerId, customerFirstName, customerLastName FROM customer WHERE " 
             + "customerFirstName LIKE '" + search + "' OR customerLastName LIKE '" + search
-            + "' OR CONCAT (customerFirstName,' ', customerLastName) LIKE '" + search + "'", function (error, results, fields) {
-            if (error) {
+            + "' OR CONCAT (customerFirstName,' ', customerLastName) LIKE '" + search + "'", function (error, results, fields) { //SQL query that finds specific customer if exists based on user input and sends it to the page
+            if (error) { //checks if there is an error in creating customer, if so error is logged
                 res.write(JSON.stringify(error));
                 res.end();
             }
@@ -28,52 +28,51 @@ module.exports = function(){
         });
     }
 
-    router.get('/', function(req, res){
+    router.get('/', function(req, res){ //this function gets the necissary data for the customer page and sends it to peoplePage.handlebars
         var callbackCount = 0;
         var context = {};
-        //context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
         if (req.query.search != null) {
             const search = req.query.search.toLowerCase();
             console.log("Searching for: " + search)
-            searchPeople(res, mysql, context, complete, search);
+            searchPeople(res, mysql, context, complete, search); //calls function to get specific customer data
         }
         else {
             console.log("Get all people Called")
-            getAllPeople(res, mysql, context, complete);
+            getAllPeople(res, mysql, context, complete); //calls function to get all customer data
         }
         function complete(){
             callbackCount++;
             if(callbackCount >= 1){
-                res.render('peoplePage', context);
+                res.render('peoplePage', context); //renders peoplePage.handlebars with requested data in 'context'
             }
         }
     });
 
-    router.post('/', function(req, res){
+    router.post('/', function(req, res){ //this function adds a customer into the database
         console.log('OK', req.body.person)
         console.log(req.body)
-        var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO customer (customerFirstName, customerLastName) VALUES (?,?)";
+        var mysql = req.app.get('mysql'); //connects to database
+        var sql = "INSERT INTO customer (customerFirstName, customerLastName) VALUES (?,?)"; //SQL query that creates a new customer
         var inserts = [req.body.customerFirstName, req.body.customerLastName];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
+            if(error){ //checks if there is an error in creating customer, if so error is logged
                 console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
                 res.end();
             }else{
-                res.redirect('/people');
+                res.redirect('/people'); //refreshes people page with new customer 
             }
         });
     }); 
 
-    router.delete('/:customerId', function (req, res) {
+    router.delete('/:customerId', function (req, res) { //function to delete a customer
         console.log("Customer Deleted: " + req.params.customerId);
-        var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM customer WHERE customerId = ?";
+        var mysql = req.app.get('mysql'); //connects to database
+        var sql = "DELETE FROM customer WHERE customerId = ?"; //SQL query to delete specific customer
         var inserts = [req.params.customerId];
         sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
-            if (error) {
+            if (error) { //checks if there is an error in creating customer, if so error is logged
                 console.log(error)
                 res.write(JSON.stringify(error));
                 res.status(400);
